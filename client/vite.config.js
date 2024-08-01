c82b37bfac2e89e2687a1405854d7d7150c15ae1
Aviral Gupta <ainoxg@gmail.com>
2024-08-01 18:37:22 +0530

    connect backend to frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+const port = import.meta.env.VITE_PORT;
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: `http://localhost:${port}`,
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
