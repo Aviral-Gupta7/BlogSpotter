fb0012bfda7cc171eb6830622cbb710ffbd39830
Aviral Gupta <ainoxg@gmail.com>
2024-08-06 22:06:23 +0530

    vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-const port = import.meta.env.VITE_PORT;
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: `http://localhost:${port}`,
-        secure: false,
+export default ({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd()));
+
+  const port = process.env.VITE_BACKEND_PORT;
+  const target = `http://localhost:${port}`;
+
+  return defineConfig({
+    server: {
+      proxy: {
+        "/api": {
+          target: target,
+          secure: false,
+        },
       },
     },
-  },
-  plugins: [react()],
-});
+    plugins: [react()],
+  });
+};
+// https://vitejs.dev/config/
